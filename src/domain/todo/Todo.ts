19980caddcd53545062dc3819958c8f4efde6bdd
Rogério Munhoz <rogerio.j.munhoz@gmail.com>
2019-11-08 16:42:15 -0300

    Change `buildTodo` so it deals with string _ids

--- src/domain/todo/Todo.ts
+++ src/domain/todo/Todo.ts
@@ -4,7 +4,12 @@ import { ToDo } from '../../../generated/todo_pb'
 
 export function buildTodo (serialized: SerializedTodo) {
   const todo = new ToDo()
-  todo.setId(serialized._id.toHexString())
+
+  const id = typeof serialized._id === 'string'
+    ? serialized._id
+    : serialized._id.toHexString()
+
+  todo.setId(id)
   todo.setTitle(serialized.title)
   todo.setDescription(serialized.description)
   todo.setDone(serialized.done)
